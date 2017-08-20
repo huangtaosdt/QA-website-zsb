a0a6d4af38f8255e60e674c1130c26f3e65666b2
ht <huangtaosdt@163.com>
2017-08-20 23:26:46 +0800

    Aug 20,Sunday

--- app/hello.py
+++ app/hello.py
@@ -1,31 +1,84 @@
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import abort
-from flask import redirect
-from flask.ext.script import Manager
-
-
-app = Flask(__name__)
-manager=Manager(app)
-
-@app.route('/')
-def index():
-    # user_agent=request.headers.get('User-Agent')
-    # return '<h1> browser:%s!</h1>' %user_agent
-    # response = make_response('<h1> Cookie!</h1>')
-    # response.set_cookie('answer','42')
-    return redirect("http://www.examsssple.com")
-
-@app.route('/user/<id>')
-def get_user(id):
-    user=id
-
-    if not user:
-        abort(404)
-    return '<h1>Hello,%s</h1>'%user
-
-
-if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+# from flask import Flask,render_template,session,redirect,url_for,flash
+# from flask_script import Manager,Shell
+# from flask_bootstrap import Bootstrap
+# from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash,check_password_hash
+# import os
+# #配置sqlite数据库
+# basedir=os.path.abspath(os.path.dirname(__file__))
+#
+# app = Flask(__name__)
+# app.config['SECRET_KEY']='hard to guess string'
+# app.config['SQLALCHEMY_DATABASE_URI']=\
+#     'sqlite:///'+os.path.join(basedir,'data.sqlite')
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+#
+# db=SQLAlchemy(app)
+# manager=Manager(app)
+# bootstrap=Bootstrap(app)
+#
+#
+# class Role(db.Model):
+#     __tablename__='roles'
+#     id=db.Column(db.Integer,primary_key=True)
+#     name=db.Column(db.String(64),unique=True)
+#
+#     users=db.relationship('User',backref='role')
+#
+#     def __repr__(self):
+#         return '<Role %r>' % self.name
+#
+# class User(db.Model):
+#     __tablename__='users'
+#     id=db.Column(db.Integer,primary_key=True)
+#     username=db.Column(db.String(64),unique=True,index=True)
+#     password_hash=db.Column(db.String(128))
+#     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+#
+#     @property
+#     def password(self):
+#         raise AttributeError('password is not a readable attribute')
+#
+#     @password.setter
+#     def password(self,password):
+#         self.password_hash=generate_password_hash(password)
+#
+#     def verify_password(self,password):
+#         return check_password_hash(self.password_hash,password)
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+#
+#
+#
+# def make_shell_context():
+#     return dict(app=app,db=db,Role=Role,User=User)
+# manager.add_command('shell',Shell(make_context=make_shell_context))
+#
+#
+#
+#
+#
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html',name=name)
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+#     # manager.run()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
