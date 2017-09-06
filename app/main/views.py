6aa280a5b9529d37b46956477d2116a2f4cf7d34
ht <huangtaosdt@163.com>
2017-09-06 21:23:36 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/PycharmProject.iml
    #	app/__init__.py
    #	app/auth/__init__.py
    #	app/auth/forms.py
    #	app/models.py
    #	app/templates/auth/login.html
    #	app/templates/base.html
    #	app/templates/index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,23 +1,99 @@
-
+import os
 from  datetime import datetime
-from flask import render_template,session,redirect,url_for
+from flask import render_template, session, redirect, url_for, abort, flash, request
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from  .. import db
-from  ..models import User
-
+from ..models import User, Role, Post, Permission
+from flask_login import login_required, current_user
+from ..decorators import admin_required
+from werkzeug.utils import secure_filename
 @main.route('/',methods=['GET','POST'])
 def index():
-    form=NameForm()
-    if form.validate_on_submit():
-        user=User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user=User(username=form.name.data)
-            db.session.add(user)
-            session['known']=False
-        else:
-            session['known']=True
-        session['name']=form.name.data
-        form.name.data=''
+    form = PostForm()
+    # if form.validate_on_submit():
+    #     user=User.query.filter_by(username=form.name.data).first()
+    #     if user is None:
+    #         user=User(username=form.name.data)
+    #         db.session.add(user)
+    #         session['known']=False
+    #     else:
+    #         session['known']=True
+    #     session['name']=form.name.data
+    #     form.name.data=''
+    #     return redirect(url_for('.index'))
+    # return render_template('index.html',form=form,name=session.get('name'),
+    #                        known=session.get('known',False),current_time=datetime.utcnow())
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user._get_current_object())
+        db.session.add(post)
         return redirect(url_for('.index'))
-    return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False),current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        # user icon
+        avatar = request.files['avatar']
+        filename = secure_filename(avatar.filename)
+        UPLOAD_FOLDER = '{}{}'.format(os.getcwd(), '/app/static/avatar/')
+        print('当前工作目录：', UPLOAD_FOLDER)
+        ALLOWED_EXTENSIONS = ['png', 'gif', 'jpeg', 'jpg', '']
+        flag = '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+        if filename and not flag:
+            flash('Avatar can only png,gif,jpeg or jpg.')
+            return redirect(url_for('.edit_profile'))
+
+        if filename:
+            file_location = '{}{}_{}'.format(UPLOAD_FOLDER, current_user.username, filename)
+            avatar.save(file_location)
+            current_user.avatar = '/static/avatar/{}_{}'.format(current_user.username, filename)
+
+        db.session.add(current_user)
+        flash('You profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
--- config.py
+++ config.py
@@ -4,23 +4,28 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_COMMIT_ON_TEAEDOEN=True
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_PORT = 587
+    MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASK_MAIL_SUBJECT_PREFIX='[FLASKY]'
-    FLASK_MAIL_SENDER='Flasky Admin <flasky@example.com>'
-    FLASK_ADMIN=os.environ.get('FLASKY_ADMIN')
-
+    # MAIL_USE_SSL=True
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = "huangtaosdt@sina.com"
+    MAIL_PASSWORD = "Huang???Tao..."
+    print(MAIL_PASSWORD)
+    print(MAIL_USERNAME)
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <huangtaosdt@sina.com>'
+    # FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = "huangtaosdt@sina.com"
     @staticmethod
     def init_app(app):
         pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.sina.com'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
