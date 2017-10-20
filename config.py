437d9877ca3a5d62c550a9d7d7236255544d7efd
ht <huangtaosdt@163.com>
2017-10-20 18:04:28 +0800

    服务器端口修改为：465，使用ssl协议

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_PORT = 25
-    MAIL_USE_TLS = True
-    # MAIL_USE_SSL=True
+    MAIL_PORT = 465
+    # MAIL_USE_TLS = True
+    MAIL_USE_SSL=True
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USERNAME = "huangtaosdt@sina.com"
